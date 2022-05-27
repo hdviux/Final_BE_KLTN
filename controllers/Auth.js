52db19ac0fd284e44da467eb7b958bc21188a8c6
hdviux <hdvu2k@gmail.com>
2022-05-27 12:16:38 +0700

    ok be3

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -209,6 +209,7 @@ const sendVerifyEmail = async (req, res, next) => {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
       port: 465,
+      secure: true,
       auth: {
         type: "OAuth2",
       },
